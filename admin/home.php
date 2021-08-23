b272d260f3953767f0690883fd9c918e0de624a7
AzzarnujiNurUkhrowi17200359 <72123588+AzzarnujiNurUkhrowi17200359@users.noreply.github.com>
2021-08-23 22:44:44 +0700

    Update home.php

--- admin/home.php
+++ admin/home.php
@@ -330,7 +330,7 @@
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                                 Jumlah Foto Dalam 1 Folder Santri</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php getCount("santri");?></div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php getCount("santri");?> Files</div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -348,7 +348,7 @@
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
                                                 Jumlah Foto Dalam 1 Folder Pondok</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php getCount("pondok");?></div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
