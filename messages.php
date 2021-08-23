68747c25d4514f84182e6e6e5c011d0a90247eb3
AzzarnujiNurUkhrowi17200359 <72123588+AzzarnujiNurUkhrowi17200359@users.noreply.github.com>
2021-08-23 15:07:51 +0700

    Update messages.php

--- messages.php
+++ messages.php
@@ -5,7 +5,7 @@ $nama_pengirim = $_POST['name'];
 $judul = $_POST['judul'];
 $nohp = $_POST['number'];
 $msg = $_POST['msg'];
-$pesan = "Nama Pengirim:$nama_pengirim<br>nEmail Pengirim:$email_pengirim<br>Nomor Hape Pengirim:$nohp<br>Pesan Pengirim :$msg";
+$pesan = "Nama Pengirim:$nama_pengirim<br>Email Pengirim:$email_pengirim<br>Nomor Hape Pengirim:$nohp<br>Pesan Pengirim :$msg";
 $mail = new PHPMailer; 
 $mail->IsSMTP();
 $mail->SMTPSecure = 'ssl'; 
@@ -15,7 +15,7 @@ $mail->Port = 465;
 $mail->SMTPAuth = true;
 $mail->Username = "azzarnuji93@pontrendarulmuttaqin.com"; //user email
 $mail->Password = "2002-10-19"; //password email 
-$mail->SetFrom($nama_pengirim,"Yapida Webmaster Automatic"); //set email pengirim
+$mail->SetFrom("azzarnuji93@pontrendarulmuttaqin.com","$nama_pengirim"); //set email pengirim
 $mail->Subject = $judul; //subyek email
 $mail->AddAddress("yapidacibarusah@gmail.com","yapidacibarusah@gmail.com");  //tujuan email
 $mail->MsgHTML($pesan);
