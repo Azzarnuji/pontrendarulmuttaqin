f2685924e5111df7f0a9e8597b0dced0fbcedf62
AzzarnujiNurUkhrowi17200359 <72123588+AzzarnujiNurUkhrowi17200359@users.noreply.github.com>
2021-08-23 22:49:33 +0700

    Update santri.php

--- admin/santri.php
+++ admin/santri.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
 <?php
         $dirname = "../assets/santri/";
         $images = glob($dirname."*.jpg");
@@ -31,3 +42,4 @@
             
         <?php endforeach;?>
     </div>
+</html>
