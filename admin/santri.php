d52cad4a0f015a9afc350bb9d136f1f21cd05bcb
AzzarnujiNurUkhrowi17200359 <72123588+AzzarnujiNurUkhrowi17200359@users.noreply.github.com>
2021-08-23 23:38:33 +0700

    Update santri.php

--- admin/santri.php
+++ admin/santri.php
@@ -294,9 +294,12 @@
                         </div>
                         <div class="card-body">
                             <script>
-                                function showModal(){
-                                    console.log("Berhsail");
-                                }
+                                $(document).ready(function(){
+                                    function showModal(){
+                                        $('#ModalSantri').modal('show');
+                                    };
+
+                                });
 
                             </script>
                             <div class="table-responsive">
@@ -341,6 +344,26 @@
 
             </div>
             <!-- End of Main Content -->
+            <!-- modal -->
+            <div class="modal fade" id="ModalSantri" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                    </div>
+                </div>
+            </div>
 
             <!-- Footer -->
             <footer class="sticky-footer bg-white">
