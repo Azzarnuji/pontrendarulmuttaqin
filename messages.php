4d4b7c829f556d625a1a32a0d2cd709fc6f658fc
AzzarnujiNurUkhrowi17200359 <72123588+AzzarnujiNurUkhrowi17200359@users.noreply.github.com>
2021-08-23 14:20:17 +0700

    ALL EDIT

--- messages.php
+++ messages.php
@@ -1,9 +1,20 @@
 <?php
-$to = "yapidacibarusah@gmail.com";;
-$subject = "PONPES DARUL MUTTAQIN";
+include "classes/class.phpmailer.php";
+$nama_pengirim = $_POST['email'];
 $msg = $_POST['msg'];
-$headers = "From: ". $_POST['email']. "\r\n" .
-"CC: somebodyelse@example.com";
-
-mail($to,$subject,$msg,$headers);
+$mail = new PHPMailer; 
+$mail->IsSMTP();
+$mail->SMTPSecure = 'ssl'; 
+$mail->Host = "smtp.gmail.com"; //host masing2 provider email
+$mail->SMTPDebug = 2;
+$mail->Port = 465;
+$mail->SMTPAuth = true;
+$mail->Username = "azzarnuji93@pontrendarulmuttaqin.com"; //user email
+$mail->Password = "2002-10-19"; //password email 
+$mail->SetFrom($nama_pengirim,"Nama pengirim"); //set email pengirim
+$mail->Subject = "Testing"; //subyek email
+$mail->AddAddress("yapidacibarusah@gmail.com","nama email tujuan");  //tujuan email
+$mail->MsgHTML("Testing...");
+if($mail->Send()) echo "Message has been sent";
+else echo "Failed to sending message";
 ?>
