3159223730c009903848ee2b5f1ae263a1d496b8
AzzarnujiNurUkhrowi17200359 <72123588+AzzarnujiNurUkhrowi17200359@users.noreply.github.com>
2021-08-23 22:38:41 +0700

    Update home.php

--- admin/home.php
+++ admin/home.php
@@ -319,7 +319,7 @@
                                 if( $files2 ) {
                                     $filecount = count($files2);
                                 }
-
+                                var_dump($directory);
                                 var_dump($filecount . "files ");
                             }
 
