243384606c6683588ec9b974578133cda09f78df
AzzarnujiNurUkhrowi17200359 <72123588+AzzarnujiNurUkhrowi17200359@users.noreply.github.com>
2021-08-23 22:36:19 +0700

    Update home.php

--- admin/home.php
+++ admin/home.php
@@ -305,13 +305,33 @@
 
                         <!-- Earnings (Monthly) Card Example -->
                         <div class="col-xl-3 col-md-6 mb-4">
+                        <?php
+                            function getCount($folder){
+
+                                // Set the current working directory
+                                $directory = getcwd()."assets/".$folder;
+
+                                // Initialize filecount variavle
+                                $filecount = 0;
+
+                                $files2 = glob( $directory ."*" );
+
+                                if( $files2 ) {
+                                    $filecount = count($files2);
+                                }
+
+                                return $filecount . "files ";
+                            }
+
+                        ?>
+
                             <div class="card border-left-primary shadow h-100 py-2">
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                                 Jumlah Foto Dalam 1 Folder Santri</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php getCount("santri");?></div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-calendar fa-2x text-gray-300"></i>
