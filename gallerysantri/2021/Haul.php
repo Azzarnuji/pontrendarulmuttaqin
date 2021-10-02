6b58f4e01e6acca545bc2a352ada07525ddfc2c0
AzzarnujiNurUkhrowi17200359 <72123588+AzzarnujiNurUkhrowi17200359@users.noreply.github.com>
2021-10-02 22:15:25 +0700

    Create Haul.php

---
+++ gallerysantri/2021/Haul.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Pontren Darul Muttaqin</title>
+	<meta http-equiv="refresh"
+		content="5; url = https://haul.pontrendarulmuttaqin.com" />
+</head>
+
+<body>
+	<h1 style="text-align:center;color:green;">
+	Pontren Darul Muttaqin
+</h1>
+	<p style="text-align:center;">
+	Halaman Akan Di Alihkan Ke Haul Pontren Darul Muttaqin, Dalam 5 Detik. <br><a href="https://haul.pontrendarulmuttaqin.com">Atau Klik Disini</a></p>
+   
+</body>
+
+</html>
