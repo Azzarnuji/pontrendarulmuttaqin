0a077f75f93f5612b37f5eb57b452a0822daf5da
AzzarnujiNurUkhrowi17200359 <72123588+AzzarnujiNurUkhrowi17200359@users.noreply.github.com>
2021-10-05 12:28:24 +0700

    Update index.php

--- database/index.php
+++ database/index.php
@@ -1,7 +1,9 @@
 <?php
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Credentials: true");
 header("Content-Type: text/html; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: GET,POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 require_once "function/Init.php";
