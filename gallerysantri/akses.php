d98d736e1a2b1ca8ce5cc2c310e2e1a04d8c0685
AzzarnujiNurUkhrowi17200359 <72123588+AzzarnujiNurUkhrowi17200359@users.noreply.github.com>
2021-11-04 02:48:21 +0700

    Update akses.php

--- gallerysantri/akses.php
+++ gallerysantri/akses.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: http://10.10.10.8/project");
+header("Access-Control-Allow-Origin: http://10.10.10.8/");
 header("Access-Control-Allow-Headers: access");
 header("Access-Control-Allow-Credentials: true");
 header("Content-Type: text/html; charset=UTF-8");
