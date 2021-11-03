bec323ed8556eef02ec0c54b18836bdbda6fc2e8
AzzarnujiNurUkhrowi17200359 <72123588+AzzarnujiNurUkhrowi17200359@users.noreply.github.com>
2021-11-04 02:37:32 +0700

    Add Header

---
+++ gallerysantri/akses.php
@@ -0,0 +1,10 @@
+<?php
+header("Access-Control-Allow-Origin: 10.10.10.8");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Credentials: true");
+header("Content-Type: text/html; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+?>
